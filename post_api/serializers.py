4b56edf8061a996e06f6f95f554808f20ee45250
Gaius <admin@MacBook-Pro.local>
2025-11-26 02:15:58 +0000

    Product serializer updated to manage images

--- post_api/serializers.py
+++ post_api/serializers.py
@@ -3,16 +3,29 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductListSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
+        
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
 
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 class ProductWriteSerializer(serializers.ModelSerializer):
     #accept category by id
